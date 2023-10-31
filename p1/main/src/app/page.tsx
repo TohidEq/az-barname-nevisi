ffb66c945224fd1f43a36fa3155eb2c17227d246
TohidEq <TohidEq@gmail.com>
2023-10-31 23:44:46 +0330

    p1-main Design

--- p1/main/src/app/page.tsx
+++ p1/main/src/app/page.tsx
@@ -1,7 +1,60 @@
+"use client";
 export default function Home() {
   return (
-    <main className="bg-blue-500 text-red-600">
-      tes 12 321 431 510 4021930210 303210t
+    <main className="blog page ">
+      <article className="blog-post ">
+        <div className="post-title ">
+          <h1 className=" drop-shadow-glow">Main App</h1>
+          <span>
+            Lotfan maghaadire khaaste shode raa be dorosti vaared konid!(faghat
+            adaad)
+          </span>
+        </div>
+
+        <section className="post-section">
+          <form action="" autoComplete="flase" className="f-form">
+            <input
+              type="text"
+              name="i-tel"
+              className="my-i i-tel"
+              placeholder="Shomareye shoma..."
+            />
+            <input
+              type="text"
+              name="i-mokaleme"
+              className="my-i i-mokaleme"
+              placeholder="Mokaleme(min)"
+            />
+            <input
+              type="text"
+              name="i-internet"
+              className="my-i i-internet"
+              placeholder="Internet(min)"
+            />
+            <input
+              type="text"
+              name="i-payamsoti"
+              className="my-i i-payamsoti"
+              placeholder="Payam soti(tedaad)"
+            />
+            <input
+              type="text"
+              name="i-payamak"
+              className="my-i i-payamak"
+              placeholder="Payamak(tedaad)"
+            />
+
+            <button type="submit" className="my-i">
+              Submit
+            </button>
+          </form>
+        </section>
+
+        <section className="post-section">
+          <h2 className=" drop-shadow-glow">Natije</h2>
+          <span>test text</span>
+        </section>
+      </article>
     </main>
   );
 }
